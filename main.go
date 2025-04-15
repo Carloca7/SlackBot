27543e98ef7fb13fe5edebe1c777c68968bd0cf7
Carlos Javier López Casero <47333892+Carloca7@users.noreply.github.com>
2025-04-15 23:13:39 +0200

    Work in progress (not even working)

--- main.go
+++ main.go
@@ -1,11 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/joho/godotenv"
+	//"github.com/slack-go/slack"
+	"log"
+	"os"
+)
 
 func main() {
-	fmt.Printf("Hello World")
-}
 
-func initConnectionSlack() {
+	slackToken := os.Getenv("slackToken")
+	if slackToken == "" {
+		log.Fatal("No Slack token found in system environments")
+		err := godotenv.Load()
+		if err != nil {
+			log.Fatal("Error loading .env file")
+			return
+		}
+		slackToken = os.Getenv("slackToken")
+		if slackToken == "" {
+			log.Fatal("Slack token taken from .env file")
+		} else {
+			fmt.Println("No Slack token found in system environments nor .env file")
+		}
+
+	}
+
+	fmt.Println(slackToken)
 
 }
